79f1b5b5ea967de14563ac878d7155986757913f
Abhishek-76 <avinashgautam8923933406@gmail.com>
2023-04-06 13:14:12 +0530

    ant design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React from "react";
+import Task from "./Task";
+
 function App() {
-  return <div className="App">To DO List</div>;
+  return (
+    <div className="App">
+      <div className="title">To DO List</div>
+      <div>
+        <Task />
+      </div>
+    </div>
+  );
 }
 
 export default App;
